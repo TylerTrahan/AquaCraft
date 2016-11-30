44455819410ed53f263d48f782ac39a06c364a42
orvilsoft <scott@neptune-consulting.net>
2016-11-30 15:46:12 -0600

    Add files via upload

--- AquaPilot1.vb
+++ AquaPilot1.vb
@@ -268,6 +268,7 @@ Public Class AquaPilot1
         BrngPoint = MySurvey1.InverseBearing
         Debug.WriteLine("LineBearing " & frmAquaPilot.lineBearing)
         Debug.WriteLine("PointBearing " & BrngPoint)
+
         If CrossTrackDist > 0 Then
             If CrossTrackDist > OldCrossTrackDist Then
                 ' calc some percentage to control the bearing change
